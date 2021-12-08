5bc17a211835c7f65763cfa1b5a9753bebfe2530
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-07 23:36:27 -0300

    feat: adiciona componente UserDeleteModal

---
+++ components/UserDeleteModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-dialog
+    width="500"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <v-card>
+      <v-card-title>Deletar usuário</v-card-title>
+      <UserDetails class="elevation-0" :user="user" />
+      <v-divider />
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          color="primary"
+          text
+          @click="$emit('input', false)"
+        >
+          Voltar
+        </v-btn>
+        <v-btn
+          color="red"
+          outlined
+          @click="$emit('input', false)"
+        >
+          Deletar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import UserDetails from '@/components/UserDetails'
+
+export default {
+  name: 'UserDeleteModal',
+  components: {
+    UserDetails
+  },
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
