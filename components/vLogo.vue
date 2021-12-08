1ced8ea103034ee94a8f2f4e27ac33ce712b5984
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-07 23:37:28 -0300

    feat: adiciona componente vLogo

---
+++ components/vLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="img-responsive" src="~/static/logo.png" alt="Logo">
+</template>
+
+<script>
+export default {
+  name: 'VLogo'
+}
+</script>
+
+<style scoped>
+.img-responsive {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+</style>
