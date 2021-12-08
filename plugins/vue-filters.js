145bfd505b0ee8aac25da486d9b51cf989f7744e
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-08 10:20:12 -0300

    refactor: adiciona métodos de formatação de @utils aos filtros globais

--- plugins/vue-filters.js
+++ plugins/vue-filters.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
+import { formatarCPF, formatarData, formatarTelefone } from '@/utils'
 
 Vue.filter('formatarData', (value) => {
   if (/(\d{4})-(\d{2})-(\d{2})/.test(value)) {
-    const [year, month, day] = value.split('-')
-    const d = new Date(year, month - 1, day)
-    return d.toLocaleDateString()
+    return formatarData(value)
   }
 
   return value
@@ -19,7 +18,7 @@ Vue.filter('formatarCPF', (value) => {
     return value
   }
 
-  return value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+  return formatarCPF(value)
 })
 
 Vue.filter('formatarTelefone', (value) => {
@@ -31,5 +30,5 @@ Vue.filter('formatarTelefone', (value) => {
     return value
   }
 
-  return value.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3')
+  return formatarTelefone(value)
 })
