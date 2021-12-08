eeb8647e123acbb9c2a4091585cbb7e4607e1dd3
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-07 23:43:54 -0300

    feat: adiciona página principal da aplicação

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="d-flex flex-row justify-center mb-4">
+      <VLogo />
+    </div>
+    <UserDataTable />
+  </div>
+</template>
+
+<script>
+import VLogo from '@/components/vLogo'
+import UserDataTable from '@/components/UserDataTable'
+
+export default {
+  name: 'Index',
+  components: {
+    VLogo,
+    UserDataTable
+  }
+}
+</script>
