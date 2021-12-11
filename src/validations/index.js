5962a603440cf3073f3534beefdf1b822fe2935e
Mateus Fontoura <fontourasantana@gmail.com>
2021-12-11 12:02:40 -0300

    feat: adiciona caracter '.' a validação do nome

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,7 +1,7 @@
 export const user = {
   nome: [
     v => !!v || 'O nome é obrigatório',
-    v => /^[a-z áàãâéèêíìîóòõôúùûç]+$/i.test(v) || 'O nome possui caracters não permitidos',
+    v => /^[a-z áàãâéèêíìîóòõôúùûç.]+$/i.test(v) || 'O nome possui caracters não permitidos',
     v => (v && v.length <= 60) || 'O nome deve ter menos de 60 caracters'
   ],
   cpf: [
